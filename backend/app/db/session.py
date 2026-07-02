f930d066f6d15a31223d023906e366c4aaf2b49b
valeriy <portmone1@gmail.com>
2026-07-02 20:59:04 +0300

    Add database session dependency typing

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterator
+from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
@@ -10,7 +10,7 @@ engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
-def get_db() -> Iterator[Session]:
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
